612af11d4bdacbea8dbb21e44239fc1e1997c4a7
Edward <nedinsuyun@gmail.com>
2021-09-28 02:22:21 -0400

    tmp saving

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
   }
 
+  getTag() {
+    let url = this.router.url;
+    let urlArr = url.split('#');
+    return urlArr[1]? urlArr[1]: '';
+    
+  }
 }
