ae1ced14b338b4f64bb8607ac1747c90f6cabcd3
Edward <nedinsuyun@gmail.com>
2021-10-01 01:28:13 -0400

    struggling scroll function

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
+import { makeStateKey } from '@angular/platform-browser';
 import { Router } from '@angular/router';
 import { aboutText } from '../../assets/library';
 
@@ -8,16 +9,90 @@ import { aboutText } from '../../assets/library';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-
+  
+  currentPosition = window.pageYOffset;
   aboutText = aboutText;
+
   constructor(private router: Router) { }
 
   ngOnInit(): void {
   }
 
+  /* Getting current tag */
   getTag() {
     let url = this.router.url;
     let urlArr = url.split('#');
     return urlArr[1]? urlArr[1]: '';
   }
+
+  /* Scroll to specific element */
+  scrollTo(direction: string) {
+    console.log(direction);
+    let target = '';
+    if(direction == 'down'){
+      switch (this.getTag()) {
+        case 'home'   : target = "about"; break;
+        case 'about'  : target = "contact"; break;
+        case 'contact': target = "contact"; break;
+        default : target = "about"; break;
+      }
+    } else if(direction == 'up') {
+      switch (this.getTag()) {
+        case 'home'   : target = "home"; break;
+        case 'about'  : target = "home"; break;
+        case 'contact': target = "about"; break;
+        default : target = "home"; break;
+      }
+    } else if(direction == 'cur') {
+      target = this.getTag();
+    }
+
+    // const debouncer = Date.now() + 2000;
+    // if(!this.scrolledTime || this.scrolledTime > debouncer) {
+    //   this.scrolledTime = Date.now();
+    // }
+    // console.log(this.scrolledTime , debouncer, debouncer - this.scrolledTime);
+    // if(this.scrolledTime > debouncer) {
+    //   return ;
+    // }
+    // console.log(1);
+    // this.scrolledTime = Date.now();
+    
+    this.router.navigate(['/Home'], { fragment: target });
+    let el = document.getElementById(target);
+    el?.scrollIntoView({behavior:'smooth'});
+    // this.sleep(1000);
+  }
+
+  sleep(ms: number) {
+    const wakeUpTime = Date.now() + ms;
+    while(Date.now() < wakeUpTime) { }
+  }
+
+  scrolledTime = Date.now();
+  wakeUpTime = Date.now();
+
+  @HostListener('window:scroll', ['$event'])
+  scrolled(e:any) {
+    console.log(this.scrolledTime, this.wakeUpTime, this.scrolledTime < this.wakeUpTime)
+    // while(this.scrolledTime < this.wakeUpTime) {
+    //   this.scrolledTime = Date.now();
+    //   this.scrollTo('cur');
+    // }
+
+    let scroll = e.target.scrollingElement.scrollTop;
+    console.log(scroll, this.currentPosition, scroll > this.currentPosition);
+    if (scroll > this.currentPosition) { // scroll down
+      // console.log('down');
+      this.scrollTo('down');
+    } else { // scroll up
+      // console.log('up');
+      this.scrollTo('up');
+    }
+    this.currentPosition = scroll;
+
+    this.scrolledTime = Date.now();
+    this.wakeUpTime = this.scrolledTime + 3000;
+  }
+  
 }
--- src/assets/library.ts
+++ src/assets/library.ts
@@ -1,7 +1,8 @@
 
 export const aboutText = 
-`<h1 class="h1-900">Hello I'm <span class="whitening">ED</span>ward, a <span class="whitening">DE</span>veloper.</h1>
-<h3>As a Frontend Developer, I have wide range of programming skills and experiences including Angular, React jQuery, to name but a few.<br />
-Feel free to take a look at my profile, resume, and projects and 
-if you're interested, please let me know by scrolling down.<h3>
+`<h1 class="font-900 text-center">Hello, I'm <span class="whitening">ED</span>ward, a <span class="whitening">DE</span>veloper.</h1>
+<h3 class="text-center">As a <span class="whitening">Frontend Developer</span>, I have wide range of programming skills and experiences including <span class="whitening">Angular, React, jQuery,</span> to name but a few.<br /></h3>
+<h3 class="text-center">I am actively looking for a job in <span class="whitening">Toronto</span>, ON, CA area<br /></h3>
+<h3 class="text-center">Feel free to take a look at my profile, resume, and projects and 
+if you're interested, please let me know by scrolling down.<br /></h3>
 `;
