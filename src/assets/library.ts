8acdae6decb29487cd557f174b806af81f63ccc2
Edward <nedinsuyun@gmail.com>
2021-10-04 18:38:45 -0400

    pages updated

--- src/assets/library.ts
+++ src/assets/library.ts
@@ -1,6 +1,6 @@
 
 export const aboutText = 
-`<h1 class="font-900 text-center">Hello, I'm <span class="whitening">ED</span>ward, a <span class="whitening">DE</span>veloper.</h1>
+`<h1 class="font-900 text-center">Hello, I'm <span class="whitening">ED</span>ward, a <span class="whitening">DE</span>veloper.</h1><br />
 <h3 class="text-center">As a <span class="whitening">Frontend Developer</span>, I have wide range of programming skills and experiences including <span class="whitening">Angular, React, jQuery,</span> to name but a few.<br /></h3>
 <h3 class="text-center">I am actively looking for a job in <span class="whitening">Toronto</span>, ON, CA area<br /></h3>
 <h3 class="text-center">Feel free to take a look at my profile, resume, and projects and 
