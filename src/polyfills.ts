3565b07696ea2b5e21ea6f8733f6dc5f8756aae7
Edward <nedinsuyun@gmail.com>
2021-10-07 15:10:57 -0400

    ie support task done

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -1,3 +1,4 @@
+
 /**
  * This file includes polyfills needed by Angular and is loaded before the app.
  * You can add your own extra polyfills to this file.
@@ -17,18 +18,31 @@
 /***************************************************************************************************
  * BROWSER POLYFILLS
  */
-
+ import 'core-js/es/symbol';
+ import 'core-js/es/object';
+ import 'core-js/es/function';
+ import 'core-js/es/parse-int';
+ import 'core-js/es/parse-float';
+ import 'core-js/es/number';
+ import 'core-js/es/math';
+ import 'core-js/es/string';
+ import 'core-js/es/date';
+ import 'core-js/es/regexp';
+ import 'core-js/es/map';
+ import 'core-js/es/weak-map';
+ import 'core-js/es/set';
+ import 'core-js/es/array'; // for .includes()
 /**
  * IE11 requires the following for NgClass support on SVG elements
  */
-// import 'classlist.js';  // Run `npm install --save classlist.js`.
+import 'classlist.js';  // Run `npm install --save classlist.js`.
 
 /**
  * Web Animations `@angular/platform-browser/animations`
  * Only required if AnimationBuilder is used within the application and using IE/Edge or Safari.
  * Standard animation support in Angular DOES NOT require any polyfills (as of Angular 6.0).
  */
-// import 'web-animations-js';  // Run `npm install --save web-animations-js`.
+import 'web-animations-js';  // Run `npm install --save web-animations-js`.
 
 /**
  * By default, zone.js will patch all possible macroTask and DomEvents
