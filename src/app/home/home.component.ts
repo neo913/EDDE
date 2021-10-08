c2b8465023ee1d04d5f4ac36a6e97ee2bd2a8d11
Edward <nedinsuyun@gmail.com>
2021-10-07 22:13:18 -0400

    logic_fix

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,7 +1,14 @@
-import { AfterViewInit, Component, Host, HostListener, OnInit } from '@angular/core';
+import { ViewportScroller } from '@angular/common';
+import { Component, HostListener, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { aboutText } from '../../assets/library';
 
+interface ModifiedRect {
+  id: string,
+  height: number,
+  width: number,
+  y: number
+}
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -11,54 +18,81 @@ export class HomeComponent implements OnInit {
   
   aboutText = aboutText;
   windowHeight = 0;
-  
-  constructor(private router: Router) { }
+  windowWidth = 0;
+  pages: ModifiedRect[] = [];
+  wakeUptime = 0;
+  direction = '';
+  lastY = 0;
+
+  constructor(private router: Router, private viewportScroller: ViewportScroller) {
+  }
 
   ngOnInit(): void {
-    this.windowHeight = document.body.clientHeight; // responsive
+    this.sizeInit();
+    this.pageInit();
+  }
+
+  sizeInit() {
+    // responsive
+    this.windowHeight = document.body.clientHeight;
+    this.windowWidth = document.body.clientWidth;
+  }
+
+  pageInit() {
+    const containers = document.getElementsByClassName('container');
+    this.pages = new Array<ModifiedRect>(containers.length);
+    for(let i=0; i< containers.length; i++) {
+      let page = containers[i].getBoundingClientRect();
+      this.pages[i] = {
+        id: containers[i].id,
+        height: page.height,
+        width: page.width,
+        y: page.y
+      };
+    }
   }
 
   /* Getting current tag */
   getTag() {
     let url = this.router.url;
     let urlArr = url.split('#');
-    return urlArr[1]? urlArr[1]: '';
+    let tag = urlArr[1]? urlArr[1]: '';
+    if(tag == '') { tag = 'home'; }
+    return tag;
   }
 
-  scrollInto(target: string) {
-    this.router.navigate(['/Home'], {fragment: target});
-    target = '#' + target;
-    document.querySelector(target)?.scrollIntoView({behavior: 'smooth', block: 'center'});
-  }
-
-  @HostListener('window:scroll', ['$event'])
-  scrollManager(event: Event) {
-
-    const home = document.querySelector('#home')?.getBoundingClientRect();
-    const about = document.querySelector('#about')?.getBoundingClientRect();
-    const skills = document.querySelector('#skills')?.getBoundingClientRect();
-    const works = document.querySelector('#works')?.getBoundingClientRect();
-    
-    let target = "";
-    let margin = this.windowHeight * 0.5;
-
-    if(home && about && skills && works) {
-
-      if(Math.abs(home.y) < margin && (this.getTag() != "" && this.getTag() != "home")) {
-        target = "home";
-      } else if(Math.abs(about.y) < margin && this.getTag() != "about") {
-        target = "about";
-      } else if(Math.abs(skills.y) < margin && this.getTag() != "skills") {
-        target = "skills";
-      } else if(Math.abs(works.y) < margin && this.getTag() != "works") {
-        target = "works"
+  scroller(id: string) {
+    if(id == 'up') {
+      if(this.getTag() != this.pages[0].id) {
+        location.hash = '#' + this.pages[this.pages.findIndex(p => p.id == this.getTag()) - 1].id;
       }
-
-      if(target != "") {
-        this.scrollInto(target);
+    } else if(id == 'down') { 
+      if(this.getTag() != this.pages[this.pages.length-1].id) {
+        location.hash = '#' + this.pages[this.pages.findIndex(p => p.id == this.getTag()) + 1].id;
       }
-      event.preventDefault();
-      event.stopPropagation();
+    } else {
+      location.hash = '#'+id; 
     }
   }
+
+  getDirection() {
+    let st = window.pageYOffset || document.documentElement.scrollTop; // Credits: "https://github.com/qeremy/so/blob/master/so.dom.js#L426"
+    st > this.lastY? this.direction = "down": this.direction = "up";
+    this.lastY = st <= 0 ? 0 : st; // For Mobile or negative scrolling
+    console.log(this.direction);
+  }
+
+  @HostListener('mousewheel', ['$event'])
+  wheelManager(event: Event) {
+    
+    this.getDirection();
+
+    if(this.wakeUptime > Date.now()) { 
+      return;
+    } else { 
+      this.wakeUptime = Date.now() + 500;
+      this.scroller(this.direction);
+    }
+  }
+
 }
